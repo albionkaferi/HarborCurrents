6440f9148e80020809f14a8f187e425f2d4ae3bc
albionkaferi <albionkaferi02@gmail.com>
2024-03-28 20:26:27 -0400

    added data transfer to webview

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,45 @@
+import { useState, useEffect, useRef } from "react";
 import { WebView } from "react-native-webview";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import Constants from "expo-constants";
 import ColorScale from "../components/ColorScale";
+import DateTimeSettings from "../components/DateTimeSettings";
+import { toLocalISOString } from "../lib/utils";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { SettingsContext } from "../contexts/SettingsContext";
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const { units } = useContext(SettingsContext);
+  const { userToken } = useContext(AuthContext);
+
+  useEffect(() => {
+    sendDataToWebView();
+  }, [date, units]);
+
+  function sendDataToWebView() {
+    webviewRef.current.postMessage({
+      token: userToken,
+      time: toLocalISOString(date),
+      units: units,
+    });
+  }
+  const webviewRef = useRef();
+
   return (
     <View style={styles.container}>
       <WebView
+        ref={webviewRef}
         style={styles.webview}
         source={{ uri: "https://harbor-currents-mapbox-website.vercel.app/" }}
+        onLoad={async () => {
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+          sendDataToWebView();
+        }}
       />
       <ColorScale />
+      <DateTimeSettings date={date} setDate={setDate} />
     </View>
   );
 }
