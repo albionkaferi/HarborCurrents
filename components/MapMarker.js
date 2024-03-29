60b98aeedc3ed7bff9c5577b9de885c41c3cb323
albionkaferi <albionkaferi02@gmail.com>
2024-03-28 20:26:59 -0400

    cleanup

--- components/MapMarker.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Marker } from "react-native-maps";
-
-function VectorPin(props) {
-  const { coord } = props;
-  return <Marker coordinate={coord} tracksViewChanges={false} />;
-}
-
-export default CarMarker = React.memo(CarPin);
